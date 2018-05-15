15aa781bb562978f356e9f154c08b7c159800db3
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-05-15 16:05:34 +0200

    Create first storage system

--- renderer.js
+++ renderer.js
@@ -1,16 +1,23 @@
-const ViewHelper = require('./js/ViewHelper.js');
+const ViewHelper = require('./js/ViewHelper.js')
+const StorageHelper = require('./js/StorageHelper.js')
 
-let viewHelper = new ViewHelper();
+let viewHelper = new ViewHelper()
+let storageHelper = new StorageHelper()
 
 document.querySelector('aside ul').addEventListener('click', (e) => {
-  let target = e.target;
+  let target = e.target
 
   if(e.target.nodeName == 'LI'){
-    viewHelper.showTab(e.target.dataset.link);
+    viewHelper.showTab(e.target.dataset.link)
   }
+})
+
+storageHelper.add({
+  name: 'Alex',
+  value: 'Rousseau'
 })
 
 
 document.getElementById('new_book_button').addEventListener('click', (e) => {
-  viewHelper.showTab('new_book');
+  viewHelper.showTab('new_book')
 })
