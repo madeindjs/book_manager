6e6734dbb2689c202332d0156a9b9bac16131f40
rehahla <bilel68@gmail.com>
2018-05-16 09:22:15 +0200

    modif de l'affichage

--- renderer.js
+++ renderer.js
@@ -5,7 +5,6 @@ let viewHelper = new ViewHelper()
 
 document.querySelector('aside ul').addEventListener('click', (e) => {
   let target = e.target
-
   if(e.target.nodeName == 'LI'){
     viewHelper.showTab(e.target.dataset.link)
   }
